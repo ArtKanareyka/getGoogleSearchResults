027a3f6c46d385f71156640381053456c772117a
ArtKanareyka <2pizzakanareyka@gmail.com>
2024-01-25 18:57:13 +0700

    Добавил вывод массива по содержаюнию в нем совпадающих слов

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getGoogleSearchResults() {
   const searchResults = document.querySelectorAll('.tF2Cxc');
 
   searchResults.forEach((result) => {
-    const title = result.querySelector('.LC20lb')?.textContent || '';
+    const title = result.querySelector('h3')?.textContent || '';
     const link = result.querySelector('a')?.getAttribute('href') || '';
     const description = result.querySelector('.VwiC3b')?.textContent || '';
     results.push({ title, link, description });
@@ -13,3 +13,19 @@ function getGoogleSearchResults() {
 }
 
 console.log(getGoogleSearchResults());
+
+function filterResultsByText(results, searchText) {
+  const filteredResults = results.filter((result) => {
+    return (
+      result.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      result.link.toLowerCase().includes(searchText.toLowerCase()) ||
+      result.description.toLowerCase().includes(searchText.toLowerCase())
+    );
+  });
+
+  return filteredResults;
+}
+
+console.log(
+  filterResultsByText(getGoogleSearchResults(), 'Вводить текст сюда')
+);
